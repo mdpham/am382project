2c7e99e6a6994b90736af02771183a8f80248855
Martin Pham <mdpham@uwaterloo.ca>
2016-04-03 15:38:05 -0400

    mutation kicks

--- three_comp_sat_feedback.m
+++ three_comp_sat_feedback.m
@@ -20,6 +20,21 @@ S = state(1); %cancer stem cells
 T = state(2); %transit-amplifying cells
 D = state(3); %terminally differentiated cells
 
+% Genetic mutation for t>100 to switch unbound/steady-state
+global mutation parameter_setting;
+if mutation
+  switch parameter_setting
+  case 0 %Currently unbound growth
+    s_1=0.1; s_2=0.3; s_3=k_0/m_0+s_1+s_2-0.25;
+		t_1=0.1; t_2=0.4; t_3=k_1/m_1+t_1+t_1-0.25;
+		c=0.25;
+  case 1 %Currently steady-state
+    s_1=0.1; s_2=0.3; s_3=k_0/m_0+s_1+s_2+0.25;
+		t_1=0.1; t_2=0.4; t_3=k_1/m_1+t_1+t_1+0.25;
+		c=0.25;
+  end;
+end
+
 %
 dS = (s_3-s_1-s_2)*S - (k_0*S^2)/(1+m_0*S);
 dT = (t_3-t_1-t_2)*T - (k_1*T^2)/(1+m_1*T) + s_2*S + (k_0*S^2)/(1+m_0*S);
--- three_comp_sf_run.m
+++ three_comp_sf_run.m
@@ -9,6 +9,8 @@ global k_0 m_0 k_1 m_1;
 k_0=0.1; m_0=0.1; k_1=0.01; m_1=0.01;
 
 % Prompt user
+% Model parameters
+global parameter_setting;
 parameter_setting=input('Model parameters: \n 0: CSC unbound \n 1: CSC steady-state \n');
 switch parameter_setting
 case 0 %Unbound growth for s,t > k0/m0,k1/m1
@@ -24,16 +26,30 @@ case 0 %Unbound growth for s,t > k0/m0,k1/m1
 		t_1=0.1; t_2=0.4; t_3=0.5;
 		c=0.25;
 end;
+% Mutation kick to change system behaviour
+global mutation;
+mutation=input('Mutation kick at t=100? \n 1: Yes \n 0: No \n');
+mutation=logical(mutation);
+
+% Initial conditions ([1 0 0])
 initial_conditions=input('Initial conditions: [CSC T D] \n');
 state0=initial_conditions;
 
 % Simulate system
 odefun=@three_comp_sat_feedback;
-tend=[0 50];
+tend=[0 200];
 [t, s]=ode45(odefun,tend,state0);
 
 % Plot results
+figure(1);
+% Use logscale since kicking to unbound will explode values
+if mutation && parameter_setting; s=log(s); end;
 plot(t,s(:,1),t,s(:,2),t,s(:,3),'linewidth',2);
-xlabel('time');ylabel('population');
+xlabel('time');
+if mutation && parameter_setting
+	ylabel('population (log)');
+else
+	ylabel('population');
+end;
 title(['CSC_0=' num2str(state0(1)) ' T_0=' num2str(state0(2)) ' D_0=' num2str(state0(3))]);
 legend('CSC','T','D');
