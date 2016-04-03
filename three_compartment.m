6768702dd6288f8678d69412297d861658a06df7
Martin Pham <mdpham@uwaterloo.ca>
2016-04-03 12:09:29 -0400

    changed model to reflect cited paper

--- three_compartment.m
+++ three_compartment.m
@@ -1,26 +1,29 @@
 function dstate = two_compartment(t,state)
 % Taken from Johnston et al.
 % http://www.ncbi.nlm.nih.gov/pubmed/17360468
-% Original model without competition/inhibition terms between CSC, TAC, D
+% Continuous model without competition/inhibition terms between CSC, TAC, D
 
 % System parameters:
-%		lambda: proliferation
-% 	alpha: self-renewal
-% 	beta: deat rate
-% 	gamma: mitotic exhaustion rate
-global LAMBDA_s ALPHA_s BETA_s;
-global LAMBDA_t ALPHA_t BETA_t;
-global BETA_d;
-global GAMMA;
+% 	_1: death
+% 	_2: differentiation
+% 	_3: self-renewal
+% 	c: mitotic exhaustion rate
+global s_1 s_2 s_3;
+global t_1 t_2 t_3;
+global c;
 
 % Current state
 S = state(1); %cancer stem cells
 T = state(2); %transit-amplifying cells
 D = state(3); %terminally differentiated cells
 
-dS=(LAMBDA_s*ALPHA_s-BETA_s)*S;
-dT=LAMBDA_s*(1-ALPHA_s)*S + LAMBDA_t*(ALPHA_t-(1-ALPHA_t)-BETA_t)*T;
-dD=LAMBDA_t*(1-ALPHA_t)*T - (GAMMA+BETA_d)*D
+
+dS = (s_3-s_1-s_2)*S;
+dT = (t_3-t_1-t_2)*T + s_2*S;
+dD = t_2*T - c*D;
+% dS=(LAMBDA_s*ALPHA_s-BETA_s)*S;
+% dT=LAMBDA_s*(1-ALPHA_s)*S + LAMBDA_t*(ALPHA_t-(1-ALPHA_t)-BETA_t)*T;
+% dD=LAMBDA_t*(1-ALPHA_t)*T - (GAMMA+BETA_d)*D
 
 dstate=[dS dT dD]';
 end
--- two_compartment.m
+++ two_compartment.m
@@ -1,23 +1,23 @@
-function dstate = two_compartment(t,state)
-% Taken from Johnston et al.
-%	http://www.ncbi.nlm.nih.gov/pubmed/17360468
-% Original model without competition/inhibition terms between CSC and CC
+% function dstate = two_compartment(t,state)
+% % Taken from Johnston et al.
+% %	http://www.ncbi.nlm.nih.gov/pubmed/17360468
+% % Original model without competition/inhibition terms between CSC and CC
 
-% System parameters:
-%		lambda: proliferation
-% 	alpha: self-renewal
-% 	beta: deat rate
-% 	gamma: mitotic exhaustion rate
-global LAMBDA_s ALPHA_s BETA_s;
-global LAMBDA_c ALPHA_c BETA_c;
-global GAMMA
+% % System parameters:
+% %		lambda: proliferation
+% % 	alpha: self-renewal
+% % 	beta: deat rate
+% % 	gamma: mitotic exhaustion rate
+% global LAMBDA_s ALPHA_s BETA_s;
+% global LAMBDA_c ALPHA_c BETA_c;
+% global GAMMA
 
-% Current state
-S = state(1); %cancer stem cells
-C = state(2); %cancer cells
+% % Current state
+% S = state(1); %cancer stem cells
+% C = state(2); %cancer cells
 
-dS=(LAMBDA_s*ALPHA_s-BETA_s)*S;
-dC=LAMBDA_s*(1-ALPHA_s)*S + LAMBDA_c*(ALPHA_c-BETA_c-GAMMA)*C;
+% dS=(LAMBDA_s*ALPHA_s-BETA_s)*S;
+% dC=LAMBDA_s*(1-ALPHA_s)*S + LAMBDA_c*(ALPHA_c-BETA_c-GAMMA)*C;
 
-dstate=[dS dC]';
-end
+% dstate=[dS dC]';
+% end
